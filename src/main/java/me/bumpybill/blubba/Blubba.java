896fe561650793af5ad131ba0ba02dccb4180af4
Lachie-Source <62362511+Lachie-Source@users.noreply.github.com>
2020-10-10 12:35:14 +1100

    mod

---
+++ src/main/java/me/bumpybill/blubba/Blubba.java
@@ -0,0 +1,13 @@
+package me.bumpybill.blubba;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Blubba implements ModInitializer {
+
+    public final static String MOD_ID = "blubba";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
